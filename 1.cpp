4f454b1fe18d6f416bb2e1576500a3c631c5780f
Stack Chan <csk3121913199@gmail.com>
2021-02-09 16:57:27 +0800

    '提交'

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+public:
+    Box(int len,int wid,int hei);
+    Box();
+    ~Box();
+    int printvolume();
+
+private:
+    int length;
+    int width;
+    int height;
+    int volume;
+};
+Box::Box(int len,int wid,int hei)
+{
+    length = len;
+    width = wid;
+    height = hei;
+}
+Box::~Box()
+{
+    cout << "Object is being deleted." << endl;
+}
+
+int Box::printvolume()
+{
+    volume = length * width * height;
+}
+
+
+int main()
+{
+
+}
